9949b74003bf371612da464ace2d95d7157a173b
Brayanbii <brayane13045@gmail.com>
2026-05-28 19:48:15 -0500

    Update logica.php

--- logica.php
+++ logica.php
@@ -1,15 +1,12 @@
 <?php
-// Configuración de zona horaria 
 date_default_timezone_set('America/Bogota');
 $hoy = date("Y-m-d H:i:s");  
 
-// Requerir dependencias de Composer
 require 'vendor/autoload.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
         $uri = getenv('MONGO_URI');
-        
         if (!$uri) {
             $uri = "mongodb+srv://brayan:5Lp8OEHxZa1ZLBYk@clusterone.xnd3dxa.mongodb.net/?appName=Clusterone"; 
         }
@@ -29,17 +26,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         
         $insertedId = $resultado->getInsertedId();
         
-        // Retorno exitoso
         header("Location: index.php?status=success&id=" . urlencode($insertedId));
         exit();
 
     } catch (Exception $e) {
-        // Retorno con error
         header("Location: index.php?status=error&msg=" . urlencode($e->getMessage()));
         exit();
     }
 } else {
-    // Protección contra acceso directo
     header("Location: index.php");
     exit();
 }
